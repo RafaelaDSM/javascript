16f8049dda82fb70dcbba5b65dcc5d9913ee6206
RafahDev <31371728+RafaelaDSM@users.noreply.github.com>
2026-03-17 20:00:03 -0300

    aula 13
    Testando estrutura de repetição while e do.

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+/*
+var c = 1
+while (c <= 6) { // while = Estrutura de repetição de teste logico no inicio.
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem ?')
+    c++
+}
+*/
+
+/* 
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
+
+var c = 1
+do { // Estrutura de repetição de teste no final.
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
